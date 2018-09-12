e16a1bb448498c51c8ce12b448d5d9cf993f358d
emmatysinger <42844450+emmatysinger@users.noreply.github.com>
2018-09-11 21:38:15 -0400

    Updated from Brython Server: 9/11/2018 9:38:15 PM

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,7 @@
 """
 fizzbuzz.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Emma Tysinger
+Credit: None
 
 Assignment:
 
@@ -44,3 +44,15 @@ Fizz
 Fizz
 Buzz
 """
+nmbr=int(input("How many numbers shall we print? "))
+fizz=int(input("For multiples of what number shall we print 'Fizz'? "))
+buzz=int(input("For multiples of what number shall we print 'Buzz'? "))
+for x in range(1,nmbr+1):
+    if x%fizz==0 and x%buzz==0:
+        print("FizzBuzz")
+    elif x%fizz==0:
+        print("Fizz")
+    elif x%buzz==0:
+        print("Buzz")
+    else:
+        print(x)
